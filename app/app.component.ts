2b72c9282dffab1f66150d929389a1f7334d6eac
Ivan Yurchenko <ivanyurchenko@MacBook-Pro-Ivan.local>
2017-01-17 00:54:27 +0200

    Added list of people

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <people-list>
+    `
 })
 export class AppComponent {
     title: String = 'Hello world!';
